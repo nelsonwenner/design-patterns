eab27a555bb4dd6a7428a48cfe6edbe25f2770f2
wenner <nelsondiasdemedeiros@gmail.com>
2021-05-12 00:35:26 -0300

    feat: add strategy example

--- strategy/src/index.ts
+++ strategy/src/index.ts
@@ -1,1 +1,40 @@
-console.log("Hello Worldd")
+interface Tax {
+  calculate(valueBase: number): number
+}
+
+class ICMS implements Tax {
+  public calculate(valueBase: number): number {
+    return valueBase * 0.2 
+  }
+}
+
+class IOF implements Tax {
+  public calculate(valueBase: number): number {
+    return valueBase * 0.3 
+  }
+}
+
+class ISS implements Tax {
+  public calculate(valueBase: number): number {
+    return valueBase * 0.5
+  }
+}
+
+class ISSQN implements Tax {
+  public calculate(valueBase: number): number {
+    return valueBase * 0.7
+  }
+}
+
+class TaxCalculator {
+  public calculate(tax: Tax, valueBase: number): number {
+    return tax.calculate(valueBase)
+  }
+}
+
+const calculator = new TaxCalculator()
+
+console.log('\nICMS: ', calculator.calculate(new ICMS(), 100))
+console.log('IOF: ', calculator.calculate(new IOF(), 100))
+console.log('ISS: ', calculator.calculate(new ISS(), 100))
+console.log('ISSQN: ', calculator.calculate(new ISSQN(), 100))
